7e5ee48de16e5dbf4fa63c2454f089a8bf6b4fe2
Gaurav Kumar Yadav <gauravyadav00729@gmail.com>
2025-07-31 13:36:07 +0530

    feat: recruiter dashboard make and coming soon page enabled

--- src/utils/AxiosInterceptor.tsx
+++ src/utils/AxiosInterceptor.tsx
@@ -11,11 +11,11 @@ interface Props {
 
 const baseOnePieceURLS =
   getEnviromentVariable() === "staginga"
-    ? "https://321ad72ff5d8.ngrok-free.app"
+    ? "https://2f6bf4373c05.ngrok-free.app"
     : // ? "http://192.168.68.113:8080/"
 
       // "https://aa3c-103-248-173-178.ngrok-free.app";
-      "https://321ad72ff5d8.ngrok-free.app";
+      "https://2f6bf4373c05.ngrok-free.app";
 
 const baseBakiURL =
   getEnviromentVariable() === "staging"
